02677c497d35f24a78369dff7434a4972244aa9d
Hakan Özler <ozler.hakan@gmail.com>
2024-05-04 22:59:52 +0300

    first initial parser attempt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+use std::fs::File;
+use std::io::BufReader;
+use xml::reader::{EventReader, XmlEvent};
+
+#[derive(Parser, Debug)]
+#[command(name = "schemaless")]
+#[command(bin_name = "schemaless")]
+#[command(version = "0.0.1")]
+#[command(author, version, about, long_about = None)]
+#[command(next_line_help = true)]
+struct SchemaArgs {
+    #[clap(value_parser = clap::value_parser!(String))]
+    file: String,
+}
+
+fn main() -> std::io::Result<()> {
+    let args = SchemaArgs::parse();
+    let file = File::open(args.file)?;
+    let file = BufReader::new(file);
+
+    let parser = EventReader::new(file);
+    let mut depth = 0;
+    for e in parser {
+        match e {
+            Ok(XmlEvent::StartElement { name, .. }) => {
+                println!("{:spaces$}+{name}", "", spaces = depth * 2);
+                depth += 1;
+            }
+            Ok(XmlEvent::EndElement { name }) => {
+                depth -= 1;
+                println!("{:spaces$}-{name}", "", spaces = depth * 2);
+            }
+            Err(e) => {
+                eprintln!("Error: {e}");
+                break;
+            }
+            _ => {}
+        }
+    }
+
+    Ok(())
 }
