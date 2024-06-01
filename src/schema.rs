459059e074ab67f58a25a278d4938f3848e2e7eb
Hakan Özler <ozler.hakan@gmail.com>
2024-06-01 10:29:36 +0300

    use switch with enums for field types

--- src/schema.rs
+++ src/schema.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use xml::attribute::OwnedAttribute;
 use xml::name::OwnedName;
 
@@ -92,76 +94,108 @@ const FIELD_TYPE_CLASSES_NAMES: [&'static str; 2] = ["solr.", "org.apache.solr.s
 
 const FIELD_KEYS: [&str; 2] = ["name", "type"];
 
+#[derive(Debug, PartialEq)]
+enum SolrFields {
+    Field,
+    CopyField,
+    FieldType,
+    Unknown(String),
+}
+
+impl FromStr for SolrFields {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "field" => Ok(SolrFields::Field),
+            "copyField" => Ok(SolrFields::CopyField),
+            "fieldType" => Ok(SolrFields::FieldType),
+            _ => Ok(SolrFields::Unknown(s.to_string())),
+        }
+    }
+}
+
 pub fn solr_parser(name: OwnedName, attributes: Vec<OwnedAttribute>) {
     let local_name = name.local_name.as_str();
     if !SCHEME_FIELDS.contains(&local_name) {
         panic!("Found unsupported schema field: {}.", &local_name)
     }
-    if &local_name == &"field" {
-        for attribute in &attributes {
-            let field_property = attribute.name.local_name.as_str();
-            if !FIELD_KEYS.contains(&field_property) && !FIELD_PROPERTIES.contains(&field_property)
-            {
-                panic!(
-                    "Found unsupported field key or property for 'field': {}.",
-                    &field_property
-                )
+    match SolrFields::from_str(&local_name) {
+        Ok(field_enum) => match field_enum {
+            SolrFields::Field => {
+                for attribute in &attributes {
+                    let field_property = attribute.name.local_name.as_str();
+                    if !FIELD_KEYS.contains(&field_property)
+                        && !FIELD_PROPERTIES.contains(&field_property)
+                    {
+                        panic!(
+                            "Found unsupported field key or property for 'field': {}.",
+                            &field_property
+                        )
+                    }
+                    if FIELD_PROPERTIES.contains(&field_property) {
+                        if attribute.value != "true" && attribute.value != "false" {
+                            panic!(
+                                "Found unsupported value '{}' for {} type in {}={}.",
+                                attribute.value,
+                                field_property,
+                                local_name,
+                                &attributes
+                                    .iter()
+                                    .find(|n| n.name.local_name == "name")
+                                    .unwrap()
+                                    .value
+                            )
+                        }
+                    }
+                }
             }
-            if FIELD_PROPERTIES.contains(&field_property) {
-                if attribute.value != "true" && attribute.value != "false" {
+            SolrFields::CopyField => {
+                let dest = &attributes
+                    .iter()
+                    .find(|n| n.name.local_name == "dest")
+                    .expect("copyField must have the dest attribute.")
+                    .value;
+                let source = &attributes
+                    .iter()
+                    .find(|n| n.name.local_name == "source")
+                    .expect("copyField must have the source attribute.")
+                    .value;
+                if dest == source {
                     panic!(
-                        "Found unsupported value '{}' for {} type in {}={}.",
-                        attribute.value,
-                        field_property,
-                        local_name,
-                        &attributes
-                            .iter()
-                            .find(|n| n.name.local_name == "name")
-                            .unwrap()
-                            .value
+                        "dest: '{}' and source: '{}' cannot share the same value in copyField.",
+                        dest, source
                     )
                 }
             }
-        }
-    } else if &local_name == &"copyField" {
-        let dest = &attributes
-            .iter()
-            .find(|n| n.name.local_name == "dest")
-            .expect("copyField must have the dest attribute.")
-            .value;
-        let source = &attributes
-            .iter()
-            .find(|n| n.name.local_name == "source")
-            .expect("copyField must have the source attribute.")
-            .value;
-        if dest == source {
-            panic!(
-                "dest: '{}' and source: '{}' cannot share the same value in copyField.",
-                dest, source
-            )
-        }
-    } else if &local_name == &"fieldType" {
-        // check a class that starts with "org.apache.solr.schema" or "solr" and has support one of FIELD_TYPE_CLASSES
-        let class_attribute: Vec<_> = attributes
-            .iter()
-            .filter(|e| &e.name.local_name == &"class")
-            .filter(|e| {
-                FIELD_TYPE_CLASSES_NAMES
+            SolrFields::FieldType => {
+                // check a class that starts with "org.apache.solr.schema" or "solr" and has support one of FIELD_TYPE_CLASSES
+                let class_attribute: Vec<_> = attributes
                     .iter()
-                    .any(|prefix| e.value.starts_with(prefix))
-            })
-            .filter(|e| {
-                FIELD_TYPE_CLASSES
-                    .iter()
-                    .any(|class_name| e.value.ends_with(class_name))
-            })
-            .cloned()
-            .collect();
-        if class_attribute.is_empty() {
-            panic!(
-                "Found an undefined class type in the fieldType decleration: {:?}",
-                &attributes
-            )
-        }
+                    .filter(|e| &e.name.local_name == &"class")
+                    .filter(|e| {
+                        FIELD_TYPE_CLASSES_NAMES
+                            .iter()
+                            .any(|prefix| e.value.starts_with(prefix))
+                    })
+                    .filter(|e| {
+                        FIELD_TYPE_CLASSES
+                            .iter()
+                            .any(|class_name| e.value.ends_with(class_name))
+                    })
+                    .cloned()
+                    .collect();
+                if class_attribute.is_empty() {
+                    panic!(
+                        "Found an undefined class type in the fieldType decleration: {:?}",
+                        &attributes
+                    )
+                }
+            }
+            SolrFields::Unknown(e) => {
+                println!("missing field, {:?}", &e)
+            }
+        },
+        Err(_) => (),
     }
 }
